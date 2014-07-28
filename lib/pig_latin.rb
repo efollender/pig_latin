c6e6ff4853e83e6837962b4f4369817452a190ca
Emma Follender <efollender@gmail.com>
2014-07-28 16:07:29 -0500

    Created modules to convert words into pig latin

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -1,5 +1,40 @@
 require "pig_latin/version"
 
 module PigLatin
-  # Your code goes here...
+  module WordDetect
+
+    def self.validate(word)
+      word !~ /\W+/ ? (return true) : (return false)
+    end
+
+    def self.checkType(word)
+      valid = WordDetect.validate(word)
+      if valid
+        if /[y]/.match(word[0])
+          return "y"
+        elsif /(^[^aeiou]+[^aeiou])/.match(word).to_s
+          return "consonant"
+        elsif /[aeiou]/.match(word[0])
+          return "vowel"
+        end
+      else
+        return "error"
+      end
+    end
+  end
+  module ConvertWord
+    def self.convert(word)
+      type = PigLatin::WordDetect.checkType(word)
+      if type == "y"
+        return (/[y]/.match(word).post_match + "yay")
+      elsif type == "consonant"
+        match = /(^[^aeiou]+[^aeiou])/.match(word)
+        return match.post_match + match.to_s + "ay"
+      elsif type == "vowel"
+        return word + "way"
+      elsif "error"
+        puts "There's been an error. Check your input"
+      end
+    end
+  end
 end
